cb29eab3b471fee43d61cc7c82618196b42660d7
nolanzhong <nolan.zhong@duke.edu>
2023-01-11 23:46:43 -0500

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Nolan Zhong on 1/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
